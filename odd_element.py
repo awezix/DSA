2bd1c662606646736a928c772344dca661e8eff5
unknown <mdarshansari3426@gmail.com>
2026-07-04 10:46:13 +0530

    added odd element

---
+++ odd_element.py
@@ -0,0 +1,13 @@
+
+# Print all even elements.
+# Print all odd elements.
+
+def odd(arr):
+    odd=[]
+    for i in arr:
+        if i % 2 != 0:
+            odd.append(i)
+    return odd
+
+array=[1,2,3,4,5,6,7,8,91,2,33,45,44]
+print(odd(array))
