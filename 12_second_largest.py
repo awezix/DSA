16bed9e02c2553b222e04dec3d471d4bb69d6691
unknown <mdarshansari3426@gmail.com>
2026-07-05 23:55:19 +0530

    added files

--- 12_second_largest.py
+++ 12_second_largest.py
@@ -18,3 +18,20 @@ def sec_largst(arr):
 arr=[21,-23,12,54,97,68,9,23,10]
 print(sec_largst(arr))
 
+
+# Find the second smallest element.
+def sec_smallest(arr):
+   smallest=float("inf")
+   s_smallest=float("inf")
+
+   for i in arr:
+      if i < smallest:
+         s_smallest=smallest
+         smallest=i
+      elif i < s_smallest and i != smallest:
+         s_smallest=i
+
+   return s_smallest
+
+arr=[1,4,32,-45,65,-48,-52,12]
+print(sec_smallest(arr))
