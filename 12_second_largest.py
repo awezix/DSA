d3ff027bc56f336cc57faf7b3222cb11799005f8
unknown <mdarshansari3426@gmail.com>
2026-07-05 23:47:00 +0530

    updated file

--- 12_second_largest.py
+++ 12_second_largest.py
@@ -1,4 +1,20 @@
 # Find the second largest element.
 
 def sec_largst(arr):
-   
+   largest=float("-inf")
+   s_largest=float("-inf")
+
+   for i in arr:
+      if i > largest:
+         s_largest=largest
+         largest=i
+
+      elif i > s_largest and i != largest:
+         s_largest=i
+        
+   return s_largest
+
+
+arr=[21,-23,12,54,97,68,9,23,10]
+print(sec_largst(arr))
+
