b2e95c9d0668fa2fb15ac7c6bbd4b5ec7e9f4ada
unknown <mdarshansari3426@gmail.com>
2026-07-06 12:21:48 +0530

    updated

--- 13_max_index.py
+++ 13_max_index.py
@@ -11,3 +11,15 @@ def max_index(arr):
 arr=[12,45,32,11,78,98,45,0]
 
 print(max_index(arr))
+
+# index of minimum element
+
+def min_index(arr):
+    min_index=0
+    for i in range(len(arr)):
+        if arr[i]<arr[min_index]:
+            min_index=i
+
+    return min_index
+
+print(min_index(arr))
