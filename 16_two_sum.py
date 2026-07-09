8b645f8635af1ff8028606e382a86fca25d4ebd0
unknown <mdarshansari3426@gmail.com>
2026-07-09 11:22:49 +0530

    updated

--- 16_two_sum.py
+++ 16_two_sum.py
@@ -1,4 +1,14 @@
 # Find all pairs whose sum equals a target.allow duplicates
 
-def twosum(arr:list,target:int):
-#      first sort the array
+def twosum(arr1:list,target:int)->list:
+
+     # now iterate through loop for two sum
+     for i in range(len(arr1)-1):
+          for j in range(i+1,len(arr1)):
+               if arr1[i] + arr1[j] == target:
+                    return i,j
+          
+
+array=[12,3,5,6,2]
+target=5
+print(twosum(array,target))
