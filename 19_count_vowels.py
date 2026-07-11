b5b71b422f4152873161914c37c5e1f9d2c60cb4
unknown <mdarshansari3426@gmail.com>
2026-07-11 12:40:32 +0530

    updates

--- 19_count_vowels.py
+++ 19_count_vowels.py
@@ -4,7 +4,7 @@ def countVowels(text:str):
     vowels=['a','e','i','o','u']
     vowels_count=0
 
-    for i in text:
+    for i in text.lower():
         if i in vowels:
             vowels_count+=1
         
@@ -13,3 +13,17 @@ def countVowels(text:str):
         
 text="programming"
 print(countVowels(text))
+
+# for consonants
+
+def consonantsCount(text:str):
+    vowels=['a','e','i','o','u']
+    consonants_counts=0
+
+    for i in text.lower():
+        if i.isalpha() and i not in vowels:
+            consonants_counts+=1
+    return consonants_counts
+
+print(consonantsCount(text))
+
